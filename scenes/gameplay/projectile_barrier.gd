7c48a9ea782ede6303d3b5b44c9065afc82409af
FuzzyFus <creeperking14@gmail.com>
2023-07-28 13:46:20 -0700

    locked rotation on cards
    oops lol
    also forgot to mention, but last push adds the float area for float_to_player()

--- scenes/gameplay/projectile_barrier.gd
+++ scenes/gameplay/projectile_barrier.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
 func _on_body_entered(ev):
-	print("collectable: " + str(ev.is_in_group("collectable")))
 	if ev.is_in_group("projectile") or ev.is_in_group("collectable"):
 		ev.queue_free()
